a5b3d85e07cc93fca2cda2a6388992cc6f090956
dev-kwon <kwonoh92@snu.ac.kr>
2021-08-24 11:11:42 +0900

    const & let & var

--- #2_day/app.js
+++ #2_day/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+/*
+let 재선언 금지, 재할당 가능
+const 재선언 금지, 재할당 금지
+var 재선언 가능, 재할당 가능
+*/
+
+const a = 5;
+const b = 2;
+let myName = "nico";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello" + myName);
+
+myName = "nicolas";
+console.log("hello" + myName);
+
